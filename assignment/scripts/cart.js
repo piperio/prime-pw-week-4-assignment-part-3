05ebebaae418493eb75d4198260a836af94d64fd
Piper Van Ness <elysian.ojala@gmail.com>
2021-08-05 15:15:04 -0500

    working on part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,13 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item){
+  basket.push(item);
+}
+
+addItem('item');
+
+console.log(basket);
