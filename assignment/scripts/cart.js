8657aeb55541b353ca4f4d62c8ceea40f40423c4
Piper Van Ness <elysian.ojala@gmail.com>
2021-08-05 21:17:09 -0500

    working on part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -11,3 +11,33 @@ function addItem(item){
 addItem('item');
 
 console.log(basket);
+
+//function any(basket){return basket.length>0;}
+//basket.some(item)
+
+//function containsItem(basket){
+  //for (let i = 0; i < basket.length; i++) {
+    //if(basket[i] === 'item'){
+      //return true;
+    //}
+  //}
+  //return false;
+//}
+//function doesBasketContainItem(item) {
+  //if (item[basket.length>0]){
+    //return true;
+//  } else {
+  //  return false;
+  //}
+  //}
+
+//console.log( 'doesBasketContainItem - should say true', doesBasketContainItem('item') );
+//console.log( 'doesBasketContainItem - should say false', doesBasketContainItem() );
+
+if (basket.length === 0){
+  console.log ('Array is empty!')
+};
+
+if (basket.length != 0){
+    console.log (true);
+};
