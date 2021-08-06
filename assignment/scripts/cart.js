1cac99fe446df4e80fe6497af4f635f3573b991d
Piper Van Ness <elysian.ojala@gmail.com>
2021-08-06 00:06:40 -0500

    finished req. parts of part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,8 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+//my comments are things I was trying.
+
 let basket = [];
 
 function addItem(item){
@@ -28,7 +30,7 @@ console.log(basket);
     //return true;
 //  } else {
   //  return false;
-  //}
+  //}con
   //}
 
 //console.log( 'doesBasketContainItem - should say true', doesBasketContainItem('item') );
@@ -41,3 +43,49 @@ if (basket.length === 0){
 if (basket.length != 0){
     console.log (true);
 };
+
+//array.forEach(function (listItems(basket){
+//  console.log(array, index);
+//});
+
+basket.pop ('item');
+console.log(basket);
+
+basket.unshift ('apples', 'oranges', 'bananas');
+console.log(basket);
+
+basket.forEach(item => console.log(item));
+
+//function listItems (basket){
+  //forEach (item =>console.log(item));
+  //}
+
+//  function listItems (basket){
+  //  for (let i = 0; i < 3; i++) {
+    //  basket[i] = listItems(){
+      //  console.log(item + i);
+  //    };
+  //  }
+  //  for (x = 0; x < 3; x++) {
+    //  listItems[i]();
+  //  }
+
+//  function listItems (for (var i = 0; i < basket.length; i++) {
+  //  console.log(listItems);
+
+//  }
+
+//function listItems(basket.length){
+  //console.log('listItems')
+//}
+
+basket.forEach(listItems);
+function listItems(item, index){
+  console.log(item);
+}
+
+function empty(basket){
+    basket.length=0;
+}
+  empty(basket);
+  console.log('Reset basket:', basket);
